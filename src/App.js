e80cb5a682b795cc0ef0cb9046616794caa0f04a
Ivonne <ivonne.scarlett01@gmail.com>
2022-03-14 13:23:31 -0600

    Estructura html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 
 function App() {
 
@@ -8,48 +8,49 @@ function App() {
     <div className="App">
 
       <main>
+        <h3 className="mt-3 text-center">PERROS</h3>
         <div className="container">
           <div className="row">
 
             <div className="col-md-12">
               <form>
                 <div className="mb-3">
-                  <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                  <label for="inputNombre" className="form-label">Nombre</label>
+                  <input type="text" className="form-control" aria-describedby="emailHelp" />
+                  <label for="inputEdad" className="form-label">Edad</label>
+                  <input type="text" className="form-control" aria-describedby="emailHelp" />
+                  <label for="inputImagen" className="form-label">Imagen</label>
+                  <input type="text" className="form-control" aria-describedby="emailHelp" />
                 </div>
-                <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="button" className="btn btn-primary">Guardar</button>                
               </form>
             </div>
 
 
             <div className="col-md-6">
-              <div className="d-flex flex-column col-10">
-                <label className="mt-3" htmlFor="name">Nombre</label>
-                <input type="text" name="name" onChange={getInputData} />
-                <label className="" htmlFor="date">Fecha</label>
-                <input type="date" className="mb-3" name="date" onChange={getInputData} />
-                <button className="Guardar" onclick={saveUser}>Guardar</button>
+              <h4 className="mt-5">Lista de perros</h4>
+              <div className="cardDogs border border-2 border-border-secondary" >
+                <img src="" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Edad</p>
+                    <a href="#" className="btn btn-primary">Eliminar</a>
+                    <a href="#" className="btn btn-primary ms-2">Guardar en Favoritos</a>
+                  </div>
               </div>
             </div>
 
             <div className="col-md-6">
-              <h2 className="mt-3">To do list</h2>
-
-              <form action="">
-                {/* <label className="" >Nombre</label>
-                <label className="">Fecha</label>
-                <input type="checkbox" /> */}
-
-              </form>
+              <h4 className="mt-5">Lista de perros favoritos</h4>
+              <div class="cardFavoritos border border-2 border-border-secondary" >
+                <img src="" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Edad</p>
+                    <a href="#" className="btn btn-primary">Eliminar</a>
+                  </div>
+              </div>
+             
             </div>
 
 
